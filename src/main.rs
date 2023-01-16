d42204fb6e679d96201df4388b12f5f14a72e4e5
Persona036 <jamessayegh9@gmail.com>
2023-01-16 16:25:06 +0200

    qemu stage

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,25 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> !{
-    loop{}
-}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+
+static HELLO: &[u8] = b"Ya mom gay";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate(){
+        unsafe{
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2) = 0xb;
+        }
+    }
+    loop{}
+}
+
+
