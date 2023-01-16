b60f93a86b38f75f968bd5a5b15ac57aa8714809
Persona036 <jamessayegh9@gmail.com>
2023-01-16 10:42:33 +0200

    le troll

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use core::panic::PanicInfo;
 #![no_std]
+#![no_main]
 
-fn main() {
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> !{
+    loop{}
 }
-
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
